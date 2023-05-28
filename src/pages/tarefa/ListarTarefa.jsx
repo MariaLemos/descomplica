09a58a6325abbff036a8f39a16c38505606459ab
Maria Lemos <maria.lemos@b2wdigital.com>
2023-05-28 17:53:07 -0300

    feat: transforma tabela em data grid

--- src/pages/tarefa/ListarTarefa.jsx
+++ src/pages/tarefa/ListarTarefa.jsx
@@ -1,44 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import Modal from '@mui/material/Modal';
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
-import CriarTarefa from './CriarTarefa';
-import EditarTarefa from './EditarTarefa';
+import { Modal, Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+import CriarTarefa from "./CriarTarefa";
+import EditarTarefa from "./EditarTarefa";
 
 //A função abaixo é usada para criar o array contendo os dados iniciais da listagem de tarefas.
 function createData(
-  idTarefa: number,
+  id: number,
   tituloTarefa: string,
   descricaoTarefa: string,
   inicioTarefa: string,
   fimTarefa: string,
   statusTarefa: string,
-  recursoTarefa: string,
+  recursoTarefa: string
 ) {
-  return { idTarefa, tituloTarefa, descricaoTarefa, inicioTarefa, fimTarefa, statusTarefa, recursoTarefa };
+  return {
+    id,
+    tituloTarefa,
+    descricaoTarefa,
+    inicioTarefa,
+    fimTarefa,
+    statusTarefa,
+    recursoTarefa,
+  };
 }
 
 //Definição do array contendo os dados iniciais da listagem de tarefas
 const initialRows = [
-  createData(1, 'Tarefa 1', 'Descrição da Tarefa 1', '2022-01-01', '2022-01-02', 'Concluída', 'Recurso 1'),
-  createData(2, 'Tarefa 2', 'Descrição da Tarefa 2', '2022-01-03', '2022-01-04', 'Em Andamento', 'Recurso 2'),
-  createData(3, 'Tarefa 3', 'Descrição da Tarefa 3', '2022-01-04', '2022-01-05', 'Em Andamento', 'Recurso 3'),
-  createData(4, 'Tarefa 4', 'Descrição da Tarefa 4', '2022-01-05', '2022-01-06', 'Em Andamento', 'Recurso 4'),
-  createData(5, 'Tarefa 5', 'Descrição da Tarefa 5', '2022-01-06', '2022-01-07', 'Em Andamento', 'Recurso 5'),
-  createData(6, 'Tarefa 6', 'Descrição da Tarefa 6', '2022-01-07', '2022-01-08', 'Aguardando', 'Recurso 6'),
+  createData(
+    1,
+    "Tarefa 1",
+    "Descrição da Tarefa 1",
+    "2022-01-01",
+    "2022-01-02",
+    "Concluída",
+    "Recurso 1"
+  ),
+  createData(
+    2,
+    "Tarefa 2",
+    "Descrição da Tarefa 2",
+    "2022-01-03",
+    "2022-01-04",
+    "Em Andamento",
+    "Recurso 2"
+  ),
+  createData(
+    3,
+    "Tarefa 3",
+    "Descrição da Tarefa 3",
+    "2022-01-04",
+    "2022-01-05",
+    "Em Andamento",
+    "Recurso 3"
+  ),
+  createData(
+    4,
+    "Tarefa 4",
+    "Descrição da Tarefa 4",
+    "2022-01-05",
+    "2022-01-06",
+    "Em Andamento",
+    "Recurso 4"
+  ),
+  createData(
+    5,
+    "Tarefa 5",
+    "Descrição da Tarefa 5",
+    "2022-01-06",
+    "2022-01-07",
+    "Em Andamento",
+    "Recurso 5"
+  ),
+  createData(
+    6,
+    "Tarefa 6",
+    "Descrição da Tarefa 6",
+    "2022-01-07",
+    "2022-01-08",
+    "Aguardando",
+    "Recurso 6"
+  ),
 ];
 
 //Componente ListarTarefa
@@ -56,114 +106,145 @@ const ListarTarefa = () => {
   //O array definido acima é setado como conteúdo do state Tarefas na renderização inicial do componente.
   useEffect(() => {
     setTarefas(initialRows);
-  },[]);
+  }, []);
 
   const handleEditar = (id) => {
     setIdTarefaSelecionada(id);
 
     //Objeto local para armazenamento da tarefa filtrada de acordo com a seleção do usuário
-    let tarefaParaEditar = tarefas.filter(obj => {
-      return obj.idTarefa === id;
+    let tarefaParaEditar = tarefas.filter((obj) => {
+      return obj.id === id;
     })[0];
 
     //Atribuição do Objeto local, setado acima, ao state Tarefa
     setTarefa(tarefaParaEditar);
 
     //Seta como true o state responsável pela exibição do Model de Editar Tarefa
-    setOpenEditar(true)
+    setOpenEditar(true);
   };
 
   const handleDeletar = (id) => {
-    setTarefas(current =>
-      current.filter(tarefa => {
-        return tarefa.idTarefa !== id;
-      }),
+    setTarefas((current) =>
+      current.filter((tarefa) => {
+        return tarefa.id !== id;
+      })
     );
   };
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "descricaoTarefa",
+      headerName: "Descrição",
+      width: 300,
+      editable: true,
+    },
+    {
+      field: "inicioTarefa",
+      headerName: "Data de Início",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "fimTarefa",
+      headerName: "Data de Finalização",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "statusTarefa",
+      headerName: "Status",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "recursoTarefa",
+      headerName: "Recurso",
+      width: 150,
+      editable: true,
+    },
 
-    return(
+    {
+      field: "editar",
+      type: "actions",
+      width: 80,
+      getActions: (params) => [
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => handleEditar(params.id)}
+        >
+          <EditIcon fontSize="small" />
+        </Button>,
+      ],
+    },
+    {
+      field: "deletar",
+      type: "actions",
+      width: 80,
+      getActions: (params) => [
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => handleDeletar(params.id)}
+        >
+          <DeleteIcon fontSize="small" />
+        </Button>,
+      ],
+    },
+  ];
+
+  return (
     <>
-    <Card>
-        <CardHeader
-          title="Tarefas"
-          subheader="Listagem de Tarefas"
-        /> 
+      <Card sx={{ height: "calc(100vh - 64px)" }}>
+        <CardHeader title="Tarefas" subheader="Listagem de Tarefas" />
         <CardContent>
-            <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                <TableHead>
-                <TableRow>
-                    <TableCell>#</TableCell>
-                    <TableCell>Título</TableCell>
-                    <TableCell align="right">Descrição</TableCell>
-                    <TableCell align="right">Data de Início</TableCell>
-                    <TableCell align="right">Data de Finalização</TableCell>
-                    <TableCell align="right">Status</TableCell>
-                    <TableCell align="right">Recurso</TableCell>
-                    <TableCell align="left"></TableCell>
-                    <TableCell align="left"></TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {tarefas.map((row, indice) => (
-                    <TableRow
-                    key={indice}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                          {row.idTarefa}
-                      </TableCell>
-                      <TableCell component="th" scope="row">
-                          {row.tituloTarefa}
-                      </TableCell>
-                      <TableCell align="right">{row.descricaoTarefa}</TableCell>
-                      <TableCell align="right">{row.inicioTarefa}</TableCell>
-                      <TableCell align="right">{row.fimTarefa}</TableCell>
-                      <TableCell align="right">{row.statusTarefa}</TableCell>
-                      <TableCell align="right">{row.recursoTarefa}</TableCell>
-                      <TableCell align="center">
-                        <Button variant="contained" color="success" onClick={() => handleEditar(row.idTarefa)}><EditIcon fontSize="small" /></Button>            
-                      </TableCell>
-                      <TableCell align="center">
-                        <Button variant="contained" color="error" onClick={() => handleDeletar(row.idTarefa)}><DeleteIcon fontSize="small" /></Button>            
-                      </TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-            </TableContainer>
+          <DataGrid rows={tarefas} columns={columns} />
         </CardContent>
         <CardActions>
-            <Button size="small" variant="contained" onClick={handleOpen}>Criar Tarefa</Button>
-            <Button size="small" variant="outlined">Cancelar</Button>
-      </CardActions> 
-    </Card>
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <div>
-          <CriarTarefa handleClose={handleClose} tarefas={tarefas} setTarefas={setTarefas} />
-        </div>
-      </Modal>  
-    </div>
-    <div>
-      <Modal
-        open={openEditar}
-        onClose={handleCloseEditar}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <div>
-          <EditarTarefa handleCloseEditar={handleCloseEditar} idTarefaSelecionada={idTarefaSelecionada} tarefas={tarefas} tarefa={tarefa} setTarefas={setTarefas} />
-        </div>
-      </Modal>  
-    </div>
-  </>    
- );
+          <Button size="small" variant="contained" onClick={handleOpen}>
+            Criar Tarefa
+          </Button>
+          <Button size="small" variant="outlined">
+            Cancelar
+          </Button>
+        </CardActions>
+      </Card>
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <div>
+            <CriarTarefa
+              handleClose={handleClose}
+              tarefas={tarefas}
+              setTarefas={setTarefas}
+            />
+          </div>
+        </Modal>
+      </div>
+      <div>
+        <Modal
+          open={openEditar}
+          onClose={handleCloseEditar}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <div>
+            <EditarTarefa
+              handleCloseEditar={handleCloseEditar}
+              idTarefaSelecionada={idTarefaSelecionada}
+              tarefas={tarefas}
+              tarefa={tarefa}
+              setTarefas={setTarefas}
+            />
+          </div>
+        </Modal>
+      </div>
+    </>
+  );
 };
- 
+
 export default ListarTarefa;
